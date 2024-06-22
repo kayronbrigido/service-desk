fa7e5b07bdbab37e61e0724026da267ba10384dd
Kayron <setokayron@hotmail.com>
2024-06-21 21:48:34 -0300

    fix: route redirection, single theme

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,12 +1,13 @@
 'use client'
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
+import { useLocale } from 'next-intl';
 
 export default function Home() {
   const router = useRouter();
+  const locale = useLocale();
 
   useEffect(() => {
-    router.replace('login')
-
-  }, [router]);
+    router.replace(`/${locale}/login`);
+  }, [router, locale]);
 }
